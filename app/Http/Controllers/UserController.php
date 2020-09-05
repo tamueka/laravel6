80be7d5b5c580d0728a713d959b5b8fbe33776f6
Samuel_Ruiz <samuelruiz@ctnaval.com>
2020-09-05 13:14:34 +0200

    php artisan make:controller UserController + routes and views whit controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,17 +7,7 @@ use DB;
 
 class UserController extends Controller
 {
-
-    public function show($name)
-    {
-        $user = DB::table('users')->where('name', $name)->first();
-
-        //dd($user);
-        return view('users', ['user'=> $user]);
-    }
-
-    public function saluda(){
-        return view('users', ['name' => 'samuel']);
-        /* return view('users')->with('name', 'Ignacio'); */
+    public function show ($id) {
+        return 'UserID ' .$id;
     }
 }
